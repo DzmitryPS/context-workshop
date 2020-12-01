d586db5378d32b3b4aeaba63971fb292d3e5960f
Marc <marcllopis89@gmail.com>
2020-12-01 11:16:06 +0100

    Adds hooks to use context

--- src/components/Final.js
+++ src/components/Final.js
@@ -1,33 +1,21 @@
-import React from 'react';
-
-// const Final = props =>
-//   <div>
-//     <p>Last things on Final.js</p>
-//     <p>NAME: {props.name}</p>
-//     <p>AGE: {props.age}</p>
-//     <button onClick={props.action}>Birthday time!</button>
-//     <hr />
-//   </div>
-
-
-// This component will be consuming our context so we need to import it in order to use it
+import React, { useContext } from 'react';
 import { MyContext } from '../context/MyProvider';
 
-// This component will be wrapped in our context.Consumer so we have access to the global state and methods declared there.
-// It only accepts a child a function that will return some html, this function has as an argument the value of our consumer (which stores the state and our methods)
-const Final = () =>
-  <MyContext.Consumer>
-    {value => (
-      <div>
-        <p>Last things on Final.js</p>
-        <p>NAME: {value.state.name}</p>
-        <p>AGE: {value.state.age}</p>
-        <button onClick={value.getOld}>Birthday time!</button>
-        <hr />
-      </div>
-    )}
 
-  </MyContext.Consumer>
+const Final = () => {
+  const context = useContext(MyContext);
+
+  return (
+    <div>
+      <p>Last things on Final.js</p>
+      <p>NAME: {context.state.name}</p>
+      <p>AGE: {context.state.age}</p>
+      <button onClick={context.getOld}>Birthday time!</button>
+      <hr />
+    </div>
+  )
+}
+
 
 
 export default Final;
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,34 +1,28 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 
 
-// It creates our context object, later we will use it to define our Provider or Consumer
-// We need to export it because when we use the Consumer, it will be in a different file
 export const MyContext = React.createContext();
 
+const initialState = {
+  name: 'Marc',
+  age: 18
+}
 
-// We create a class component that will store the global state of the app and the methods we use
-class MyProvider extends Component {
+const MyProvider = props => {
 
-  state= {
-    name: 'Marc',
-    age: 18
-  }
+  const [state, setState] = useState(initialState)
 
-  getOld= () => this.setState({age: this.state.age +1})
+  const getOld = () => setState({
+    ...state,
+    age: state.age + 1
+  })
 
+  return (
+    <MyContext.Provider value={{state, getOld}}>
+      {props.children}
+    </MyContext.Provider>
+  )
 
-  render(){
-    // We need to return the Provider of our Context, this is a component that accepts a prop named value. This value is an object that could contain a state and any method we need.
-    // This component will work as a wrapper for our app, in order to use it as a wrapper, we need to render the props.children
-    return (
-      <MyContext.Provider value={{
-        state: this.state,
-        getOld: this.getOld
-      }}>
-        {this.props.children}
-      </MyContext.Provider>
-    )
-  }
 }
 
 export default MyProvider;
